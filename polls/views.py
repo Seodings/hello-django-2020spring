835ab2a4e671f903c5a71ae78c640bd707631312
Seodings <60088401+Seodings@users.noreply.github.com>
2020-04-06 00:05:55 +0900

    update views, models

--- polls/views.py
+++ polls/views.py
@@ -21,6 +21,8 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 '''
+
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
@@ -28,9 +30,11 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Question.objects.order_by('pub_date')[:5]
 
+
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
 
 class ResultsView(generic.DetailView):
     model = Question
